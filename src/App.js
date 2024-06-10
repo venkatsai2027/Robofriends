c247be565057f59d440a26ea22d000d2cc1ccc5e
venkatsai2027 <venkatsai2027@gmail.com>
2024-06-10 19:43:27 +0530

    project files added

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import 'tachyons';
+import './App.css';
+
+
+class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+			robots : [],
+			searchfield : ''
+		}
+	}
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({robots: users}));
+	}
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value})
+	}
+	render() {
+		const filteredRobots = this.state.robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		})
+		return (
+		<div className = 'tc'>
+			<h1>Robofriends</h1>
+			<SearchBox searchChange = {this.onSearchChange}/>
+			<Scroll>
+				<CardList robots = {filteredRobots} />
+			</Scroll>
+		</div>
+
+		);
+	}
+}
+
+export default App;
